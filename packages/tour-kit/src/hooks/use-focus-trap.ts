cf101e32abd028dc58d22dea5d2e06e989d5f259
Emanuele Buccelli <escapemanuele@gmail.com>
2022-01-04 15:57:04 +0100

    focus-trap to use @wordpress/dom

--- packages/tour-kit/src/hooks/use-focus-trap.ts
+++ packages/tour-kit/src/hooks/use-focus-trap.ts
@@ -1,7 +1,11 @@
 /**
+ * WordPress dependencies
+ */
+import { focus } from '@wordpress/dom';
+/**
  * External Dependencies
  */
-import { useEffect, useCallback, useState } from '@wordpress/element';
+import { useEffect, useCallback, useRef } from '@wordpress/element';
 
 /**
  * A hook that constraints tabbing/focus on focuable elements in the given element ref.
@@ -9,61 +13,42 @@ import { useEffect, useCallback, useState } from '@wordpress/element';
  * @param ref React.MutableRefObject< null | HTMLElement >
  */
 const useFocusTrap = ( ref: React.MutableRefObject< null | HTMLElement > ): void => {
-	const [ firstFocusableElement, setFirstFocusableElement ] = useState< HTMLElement | undefined >();
-	const [ lastFocusableElement, setLastFocusableElement ] = useState< HTMLElement | undefined >();
+	const timeoutId = useRef< number >();
 
 	const handleTrapFocus = useCallback(
 		( event ) => {
-			let handled = false;
-
-			if ( event.key === 'Tab' ) {
-				if ( event.shiftKey ) {
-					// Shift + Tab
-					if ( document.activeElement === firstFocusableElement ) {
-						lastFocusableElement?.focus();
-						handled = true;
-					}
-				} else if ( document.activeElement === lastFocusableElement ) {
-					// Tab
-					firstFocusableElement?.focus();
-					handled = true;
-				}
+			const { key, shiftKey, target } = event;
+			if ( key !== 'Tab' || ! ref.current ) {
+				return;
 			}
 
-			if ( handled ) {
-				event.preventDefault();
-				event.stopPropagation();
+			const action = shiftKey ? 'findPrevious' : 'findNext';
+			const nextElement = focus.tabbable[ action ]( target ) || null;
+
+			if ( ref.current?.contains( nextElement ) ) {
+				return;
 			}
+
+			const domAction = shiftKey ? 'append' : 'prepend';
+			const { ownerDocument } = ref.current;
+			const trap = ownerDocument.createElement( 'div' );
+
+			trap.tabIndex = -1;
+			ref.current?.[ domAction ]( trap );
+			trap.focus();
+			timeoutId.current = setTimeout( () => ref.current?.removeChild( trap ) );
 		},
-		[ firstFocusableElement, lastFocusableElement ]
+		[ ref ]
 	);
 
 	useEffect( () => {
-		const focusableElementSelectors = [
-			'a[href]:not([disabled])',
-			'button:not([disabled])',
-			'textarea:not([disabled])',
-			'input[type="text"]:not([disabled])',
-			'input[type="radio"]:not([disabled])',
-			'input[type="checkbox"]:not([disabled])',
-			'select:not([disabled])',
-		];
-
-		const focusableElements = ref.current?.querySelectorAll< HTMLElement >(
-			focusableElementSelectors.toString()
-		);
-
-		if ( focusableElements && focusableElements.length ) {
-			setFirstFocusableElement( focusableElements[ 0 ] );
-			setLastFocusableElement( focusableElements[ focusableElements.length - 1 ] );
-		}
-
 		document.addEventListener( 'keydown', handleTrapFocus );
 
 		return () => {
 			document.removeEventListener( 'keydown', handleTrapFocus );
+			clearTimeout( timeoutId.current );
 		};
-	}, [ ref, handleTrapFocus ] );
+	}, [ ref, handleTrapFocus, timeoutId ] );
 };
 
 export default useFocusTrap;
