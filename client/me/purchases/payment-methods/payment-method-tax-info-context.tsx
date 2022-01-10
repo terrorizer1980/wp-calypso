bbe226327e37c1b1781863973c6491a1bf66280b
Christopher Smith <chris@chriswsmith.info>
2022-01-10 14:40:40 -0700

    Change location of useContext

--- client/me/purchases/payment-methods/payment-method-tax-info-context.tsx
+++ client/me/purchases/payment-methods/payment-method-tax-info-context.tsx
@@ -1,16 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext } from 'react';
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const [ isTaxInfoSet, setTaxInfoSet ] = useState( false );
+const TaxInfoContext = createContext( false );
 
-const TaxInfoContext = createContext( { isTaxInfoSet, setTaxInfoSet } );
-
-const TaxInfoContextProvider = ( { children } ) => {
-	return (
-		<TaxInfoContextProvider.Provider value={ { isTaxInfoSet, setTaxInfoSet } }>
-			{ ...children }
-		</TaxInfoContextProvider.Provider>
-	);
-};
-
-export { TaxInfoContext, TaxInfoContextProvider };
+export default TaxInfoContext;
